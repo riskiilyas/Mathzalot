b5c466f69f188b651cb78360b42d36653268d52d
mikhael arya <recluisen@localhost.localdomain>
2023-06-09 10:45:57 +0000

    Added game.cs to generate questions. Changed TargetFramework in Mathzalot.csproj to 7.0

---
+++ Mathgame/game.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Random;
+using System.Collections.Generic;
+
+namespace Mathzalot.Game
+{
+    public class Game
+    {
+
+        private enum Operation
+        {
+            Addition,
+            Subtraction,
+            Multiplication,
+            Division
+        }
+
+        private Operation[] mathOperations = (Operation[])Enum.GetValues(typeof(Operation));
+        private Random rng;
+
+        private List<KeyValuePair<(int,Operation,int),float>> questionsAndAnswers;
+        
+        public Game(int maxQuestions)
+        {
+            rng = new Random((int)DateTime.Now.Ticks);
+            questionsAndAnswers = new List<KeyValuePair<(int,Operation,int),float>>();
+
+            for (int i = 0; i < maxQuestions; i++)
+            {
+                QuestionGenerator(100);
+            }
+        }
+
+        public List<KeyValuePair<(int,Operation,int),float>> getQuestion(uint index)
+        {
+            return questionsAndAnswers[index];
+        }
+
+        private Operation OperationRandomizer()
+        {
+            int index = rng.Next(0, mathOperations.Length);
+
+            Operation randOp = mathOperations[index];
+
+            return randOp;
+        }
+
+        private void QuestionGenerator(int maxValue)
+        {
+            int num1 = rng.Next(0,max);
+            int num2 = rng.Next(0, max);
+            float answer = float.NaN;
+            Operation operation = OperationRandomizer();
+
+            if (operation == Operation.Addition)
+            {
+                answer = num1 + num2;
+            }
+            else if (operation == Operation.Subtraction)
+            {
+                answer = num1 - num2;
+            }
+            else if (operation == Operation.Multiplication)
+            {
+                answer = num1 * num2;
+            }
+            else if (operation == Operation.Division)
+            {
+                while(num2 == 0)
+                {
+                    num2 = rng.Next(0, max);
+                }
+                answer = num1/(float)num2;
+            }
+
+            questionsAndAnswers.Add(new KeyValuePair<(int,Operation,int),float>(num1,operation,num2),answer);
+        }
+    }
+}
